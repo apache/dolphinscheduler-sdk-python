fed760a5fa5dbdaaef16b70e4536e9389f242190
Jay Chung <zhongjiajie955@gmail.com>
2022-12-06 11:42:47 +0800

    Revert "test enabled resource center by default"
    This reverts commit 34aa71b1f369fcb9cd3cce133dc19dd13869b938.

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -21,7 +21,6 @@ import os
 
 import pytest
 
-from tests.testing.constants import JAVA_OPTS
 from tests.testing.docker_wrapper import DockerWrapper
 
 
@@ -45,11 +44,7 @@ def docker_setup_teardown():
         )
         ports = {"25333/tcp": 25333, "12345/tcp": 12345}
         container = docker_wrapper.run_until_log(
-            log="Started StandaloneServer in",
-            tty=True,
-            ports=ports,
-            # FIXME: To turn on resource center by Java properties, until support local type default.
-            environment={"JAVA_OPTS": JAVA_OPTS},
+            log="Started StandaloneServer in", tty=True, ports=ports
         )
         assert container is not None
         yield
